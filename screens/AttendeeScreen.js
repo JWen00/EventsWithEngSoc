4322918b31e8c9f1e45dc65a22a3bfbc8a550e69
JWen00 <jenniferwen00@gmail.com>
2020-04-25 19:51:43 +1000

    Refactor: Made Screen into a component & Searching Works!

--- screens/AttendeeScreen.js
+++ screens/AttendeeScreen.js
@@ -1,15 +1,32 @@
-import React, { Component, useRef } from "react";
-import { FlatList, Text, View, StyleSheet, TextInput } from "react-native";
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  Keyboard,
+  BackHandler
+} from "react-native";
+import { TouchableOpacity, ScrollView } from "react-native-gesture-handler";
 
+import chunk from "lodash/chunk";
 import { GlobalStyles } from "../constants/GlobalStyle";
 import AttendeeData from "../testData";
 import Icon from "../components/Icon";
 import Colors from "../constants/Colors";
-import { TouchableOpacity } from "react-native-gesture-handler";
+/* 
+      NOTE: 
+      You need to bind the function so it has access
+      to state and other variables in your class, 
+      not just parameters that pass when you execute  
+      Reference: https://stackoverflow.com/questions/50726014/why-we-need-to-bind-function-in-react-native
 
-// TODO: When backend is set up, AttendeeData should be fetched every 5(?) seconds or on pull down refresh.
+      To "un"focus TextInput, use ScrollView and Keyboard.dismiss 
+      Reference: https://stackoverflow.com/questions/43431896/unfocus-a-textinput-in-react-native
 
-const Card = (props) => {
+
+      */
+function Card(props) {
   return (
     <View style={styles.card}>
       <View style={styles.cardContent}>
@@ -32,116 +49,120 @@ const Card = (props) => {
       </View>
     </View>
   );
-};
+}
 
-class SearchBar extends Component {
-  constructor(props) {
-    super(props);
+const attendees = AttendeeData;
+export default class AttendeeScreen extends React.Component {
+  constructor() {
+    super();
     this.state = {
-      text: "Search",
-      data: AttendeeData,
-      isFocused: false,
+      query: "Search",
+      isFocused: false
     };
   }
 
-  filter(q) {
-    const updatedData = [];
-    const query = q.toLowerCase().trim();
-    return query;
-    // for (let i = 0; i < AttendeeData.length; i++) {
-    //   const result = AttendeeData[i].zID.match(query); // match should return a list
-    //   if (result != null) {
-    //     updatedData.push(result[0]);
-    //   }
-    // }
-    // return updatedData;
+  handleOnCardPress() {
+    //TODO
   }
 
-  render() {
+  renderSearchBar() {
     return (
-      <View>
+      <View style={styles.searchBarContainer}>
         <View style={styles.searchBarContent}>
           <View style={styles.searchBarContent}>
             <Icon size={30} name="md-search" />
           </View>
-          <TextInput
-            style={styles.searchBar}
-            onChangeText={(query) => {
+
+          <ScrollView style={styles.searchBar}>
+            <TextInput
+              onChangeText={query => {
+                this.setState({
+                  query: query
+                });
+              }}
+              onFocus={() =>
+                this.setState({
+                  query: "",
+                  isFocused: true
+                })
+              }
+              onEndEditing={() =>
+                this.setState({
+                  isFocused: false
+                })
+              }
+              value={this.state.query}
+            />
+          </ScrollView>
+          <TouchableOpacity
+            style={styles.searchBarContent}
+            onPress={() => {
               this.setState({
-                text: query,
-                data: this.filter(query),
+                query: "",
+                isFocused: false
               });
+              Keyboard.dismiss();
             }}
-            onFocus={() =>
-              this.setState({
-                text: "",
-                isFocused: true,
-                data: AttendeeData,
-              })
-            }
-            onEndEditing={() => this.setState({ isFocused: false })}
-            value={this.state.text}
-          />
-          <View style={styles.searchBarContent}>
+          >
             <Icon size={27} name="md-close" />
-          </View>
+          </TouchableOpacity>
         </View>
-
-        {/* <Text>Searched Item: {this.state.text}</Text>
-        <Text>
-          Data: {this.state.data == null ? this.state.data : "List is empty"}
-        </Text>
-        <Text>IsFocused: {this.state.isFocused ? "True" : "False"}</Text> */}
-
-        {/* <View style={styles.attendeeList}>
-          <FlatList
-            // data={this.isFocused ? AttendeeData : this.state.data}
-            data={AttendeeData}
-            extraData={this.state.data}
-            renderItem={({ item }) => (
-              <Card
-                zID={item.zID}
-                name={item.name}
-                paid={item.paid}
-                registered={item.registered}
-              ></Card>
-            )}
-          />
-        </View> */}
       </View>
     );
   }
-}
 
-export default function AttendeeScreen() {
-  return (
-    <View style={GlobalStyles.container}>
-      <View style={styles.searchBarContainer}>
-        <SearchBar />
-      </View>
+  renderSearchResult() {
+    const query = this.state.query.toLowerCase().trim();
+    return (
       <View style={styles.attendeeList}>
-        <FlatList
-          // data={this.isFocused ? AttendeeData : this.state.data}
-          data={AttendeeData}
-          renderItem={({ item }) => (
+        {attendees
+          .filter(attendee => attendee.zID.includes(query))
+          .map(filteredAttendee => (
             <Card
-              zID={item.zID}
-              name={item.name}
-              paid={item.paid}
-              registered={item.registered}
-            ></Card>
-          )}
-        />
+              zID={filteredAttendee.zID}
+              name={filteredAttendee.name}
+              paid={filteredAttendee.paid}
+              registered={filteredAttendee.registered}
+            />
+          ))}
       </View>
-    </View>
-  );
+    );
+  }
+
+  renderEntireList() {
+    return (
+      <ScrollView style={styles.attendeeList}>
+        {attendees.map((attendee, index) => (
+          <Card
+            zID={attendee.zID}
+            name={attendee.name}
+            paid={attendee.paid}
+            registered={attendee.registered}
+          />
+        ))}
+      </ScrollView>
+    );
+  }
+
+  render() {
+    return (
+      <View style={GlobalStyles.container}>
+        {this.renderSearchBar()}
+        {/* <Text>Search Query: {this.state.query}</Text> */}
+        {/* <Text>Searchinggg: {this.state.isFocused ? "True" : "False"}</Text> */}
+        {this.state.isFocused
+          ? this.renderSearchResult()
+          : this.renderEntireList()}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   searchBarContainer: {
     marginVertical: 12,
     marginHorizontal: 10,
-    textAlignVertical: "center",
+    textAlignVertical: "center"
   },
   searchBarContent: {
     display: "flex",
@@ -149,22 +170,22 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.veryLightGrey,
     flexDirection: "row",
     padding: 5,
-    borderRadius: 35,
+    borderRadius: 35
   },
   searchBarIcon: {
     top: 10,
     width: 10,
-    backgroundColor: Colors.navyBlue,
+    backgroundColor: Colors.navyBlue
   },
 
   searchBar: {
-    height: 35,
-    right: 25,
-    width: 250,
+    marginVertical: 5,
+    marginHorizontal: 5,
+    color: Colors.grey
   },
   attendeeList: {
     flex: 1,
-    padding: 5,
+    padding: 5
   },
   card: {
     elevation: 2,
@@ -175,6 +196,7 @@ const styles = StyleSheet.create({
     shadowRadius: 2,
     marginHorizontal: 4,
     marginVertical: 6,
+    borderRadius: 10
   },
   cardContent: {
     marginHorizontal: 15,
@@ -182,12 +204,12 @@ const styles = StyleSheet.create({
     marginRight: 10,
     display: "flex",
     justifyContent: "space-between",
-    flexDirection: "row",
+    flexDirection: "row"
   },
   cardIcon: {
     flexDirection: "row",
     marginHorizontal: 5,
     marginVertical: 4,
-    marginRight: 2,
-  },
+    marginRight: 2
+  }
 });
